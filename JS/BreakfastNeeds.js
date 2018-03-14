00c1aab0729a38e45cfa2cb96c0368be2c40081d
abyshukla <35761999+abyshukla@users.noreply.github.com>
2018-03-14 10:12:38 +0530

    latest from google drive

--- JS/BreakfastNeeds.js
+++ JS/BreakfastNeeds.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     // console.log($('#cowMilPrice').html(), cowMilPrice, cowMilQuantity);
 
-    var jsonData = {
+    var itemJSON = {
         "milk": {
             "Cow Milk": 21,
             "Full Cream Milk": 26,
@@ -22,22 +22,25 @@ $(document).ready(function() {
             "Butter (500g)": 225
         }
     };
-    $('#cowMilPrice').html('&#8377 ' + jsonData['milk']['Cow Milk']);
-    $('#fatMilPrice').html('&#8377 ' + jsonData['milk']['Full Cream Milk']);
-    $('#tonMilPrice').html('&#8377 ' + jsonData['milk']['Toned Milk']);
-    $('#lytMilPrice').html('&#8377 ' + jsonData['milk']['Full Toned Milk']);
-    $('#brnBrdPrice').html('&#8377 ' + jsonData['bread']['Brown Bread']);
-    $('#ataBrdPrice').html('&#8377 ' + jsonData['bread']['Atta Bread']);
-    $('#swcBrdPrice').html('&#8377 ' + jsonData['bread']['Sandwich Bread']);
-    $('#wytBrdPrice').html('&#8377 ' + jsonData['bread']['White Bread']);
-    $('#mgnBrdPrice').html('&#8377 ' + jsonData['bread']['Multi-grain Bread']);
-    $('#wytEggPrice').html('&#8377 ' + jsonData['eggs']['White Eggs']);
-    $('#kidBtrPrice').html('&#8377 ' + jsonData['butter']['Butter (10g)']);
-    $('#smlBtrPrice').html('&#8377 ' + jsonData['butter']['Butter (100g)']);
-    $('#lrgBtrPrice').html('&#8377 ' + jsonData['butter']['Butter (500g)']);
+
+    var orderJSON = {};
+
+    $('#cowMilPrice').html('&#8377 ' + itemJSON['milk']['Cow Milk']);
+    $('#fatMilPrice').html('&#8377 ' + itemJSON['milk']['Full Cream Milk']);
+    $('#tonMilPrice').html('&#8377 ' + itemJSON['milk']['Toned Milk']);
+    $('#lytMilPrice').html('&#8377 ' + itemJSON['milk']['Full Toned Milk']);
+    $('#brnBrdPrice').html('&#8377 ' + itemJSON['bread']['Brown Bread']);
+    $('#ataBrdPrice').html('&#8377 ' + itemJSON['bread']['Atta Bread']);
+    $('#swcBrdPrice').html('&#8377 ' + itemJSON['bread']['Sandwich Bread']);
+    $('#wytBrdPrice').html('&#8377 ' + itemJSON['bread']['White Bread']);
+    $('#mgnBrdPrice').html('&#8377 ' + itemJSON['bread']['Multi-grain Bread']);
+    $('#wytEggPrice').html('&#8377 ' + itemJSON['eggs']['White Eggs']);
+    $('#kidBtrPrice').html('&#8377 ' + itemJSON['butter']['Butter (10g)']);
+    $('#smlBtrPrice').html('&#8377 ' + itemJSON['butter']['Butter (100g)']);
+    $('#lrgBtrPrice').html('&#8377 ' + itemJSON['butter']['Butter (500g)']);
 
 
-    console.log(typeof jsonData['milk']['Cow Milk']);
+    console.log(typeof itemJSON['milk']['Cow Milk']);
 
 
     //this function feeds values and calculates final price
@@ -59,9 +62,9 @@ $(document).ready(function() {
                 itemType = 'butter';
                 break;
         }
-
+        console.log(this.id);
         var item = $('#' + btnId.slice(0, 6) + 'Item').html();
-        var price = jsonData[itemType][item];
+        var price = itemJSON[itemType][item];
         var quantity = parseInt($('#' + btnId.slice(0, 6) + 'Quantity').html(), 10);
         var entryTotal = parseInt($('#' + btnId.slice(0, 6) + 'Total').html().slice(1), 10);
         // var price = $('#' + btnId.slice(0, 6) + 'Price').html();
@@ -69,13 +72,28 @@ $(document).ready(function() {
             quantity += 1;
         } else if (this.id.indexOf('Less') !== -1 && quantity > 0) {
             quantity -= 1;
+        } else if (this.id.indexOf('Dozn') !== -1) {
+            quantity += 12;
+        } else if (this.id.indexOf('Thty') !== -1) {
+            quantity += 30;
+        }
+        if (quantity === 0 && orderJSON.hasOwnProperty(itemType)) {
+            delete orderJSON[itemType];
+        } else {
+            //adding orders in orderJSON
+            if (!orderJSON.hasOwnProperty(itemType)) {
+                orderJSON[itemType] = {};
+            }
+            orderJSON[itemType][item] = quantity;
         }
         $('#' + btnId.slice(0, 6) + 'Quantity').html(quantity);
         entryTotal = price * quantity;
         $('#' + btnId.slice(0, 6) + 'Total').html('&#8377 ' + entryTotal);
         console.log(item, itemType, price, quantity, entryTotal, typeof $('#' + btnId.slice(0, 6) + 'Total').html().slice(1));
     }
-
-    $('img').on('click', funcCalculateTotal);
-
+    console.log(orderJSON);
+    $('.buttonSize, .mdl-button--colored').on('click', funcCalculateTotal);
+    $('#addToCart').on('click', function() {
+        console.log(orderJSON);
+    });
 });
